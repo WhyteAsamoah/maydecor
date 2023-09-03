735db669cfc3e83c3bce7c983c2be16f6273fcc2
Japhet Kofi Appau Arthur <jajaclus3@gmail.com>
2023-09-03 16:08:15 +0200

    New Branch Upload

---
+++ public/js/seller.js
@@ -0,0 +1,119 @@
+let loader = document.querySelector('.loader');
+let user = JSON.parse(sessionStorage.user || null);
+
+const becomeSellerElement = document.querySelector('.become-seller');
+const productListingElement = document.querySelector('.product-listing');
+const applyForm = document.querySelector('.apply-form');
+const showApplyFormBtn = document.querySelector('#apply-btn');
+
+window.onload = () => {
+    if(sessionStorage.user) {
+        if(compareToken(user.authToken, user.email)) {
+            if(!user.seller) {
+                becomeSellerElement.classList.remove('hide');
+            } else{
+                loader.style.display = 'block';
+                setupProducts();
+            }
+        } else {
+            location.replace('/login');
+        }
+    } else {
+        location.replace('/login');
+    }
+}
+
+showApplyFormBtn.addEventListener('click', () => {
+    becomeSellerElement.classList.add('hide');
+    applyForm.classList.remove('hide');
+})
+
+// form submission
+const applyFormButton = document.querySelector('#apply-form-btn');
+const businessName = document.querySelector('#business-name');
+const address = document.querySelector('#business-add');
+const about = document.querySelector('#about');
+const number = document.querySelector('#number');
+const tac = document.querySelector('#terms-and-cond');
+const legitInfo = document.querySelector('#legitInfo');
+
+applyFormButton.addEventListener('click', () => {
+    if (!businessName.value.length || !address.value.length || !about.value.length || !number.value.length) {
+        showAlert('Please fill all the fields');
+    } else if (!tac.checked || !legitInfo.checked) {
+        showAlert('Please agree to the terms and conditions'); 
+    } else{
+        // making server request
+        loader.style.display = 'block';
+        sendData('/seller', {
+            name: businessName.value, 
+            address: address.value, 
+            about: about.value, 
+            number: number.value,
+            tac: tac.checked,
+            legit: legitInfo.checked,
+            email: JSON.parse(sessionStorage.user).email
+        })
+    }
+})
+
+const setupProducts = () => {
+    fetch('/get-seller-products', {
+        method: 'POST',
+        headers: new Headers({"Content-Type": "application/json"}),
+        body: JSON.stringify({email: user.email})
+    })
+    .then(res => res.json())
+    .then(data => {
+        loader.style.display = null;
+        productListingElement.classList.remove('hide');
+        if(data == 'No products found') {
+            let emptySvg = document.querySelector('.no-product-image');
+            emptySvg.classList.remove('hide');
+        } else {
+            data.forEach(product => {
+                (async() => {
+                    // UPDATES: Pencode 
+                    await getProductImages(product)
+                    // console.log(`NEW product: ${JSON.stringify(product)}`)
+                    createProduct(product)
+                })();
+            });
+        }
+    });
+}
+
+// // UPDATES: Pencode
+// const getProductImages = async (product) => {
+//     try{
+//     let fetchtRes = await fetch('/get-product-images', {
+//                         method: 'POST',
+//                         headers: new Headers({"Content-Type": "application/json"}),
+//                         body: JSON.stringify({product: product.id})
+//                     })
+    
+//         if (fetchtRes.status == 200){
+//             let jsonRes = await fetchtRes.json()
+//             let product_images = jsonRes['content']
+//             for (let i = 0; i < product_images.length; i++){
+//                 product[`img_${i}`] = product_images[i]
+//             }
+//         }
+
+//         // .then((res) => {
+//         //     if (res.status == 200){
+//         //         return res.json()
+//         //     }else {
+
+//         //     }
+//         // }).then(json => {
+            
+//         // })
+//         // .catch(ex => {
+//         //     console.log(ex)
+//         // })
+//     }
+//     catch(e){
+//         console.error(e)
+//     }
+// }
---
+++ public/js/token.js
@@ -0,0 +1,132 @@
+let char = `123abcde.fmnopqlABCDE@FJKLMNOPQRSTUVWXYZ456789stuvwxyz0!#$%&ijkrgh'*+-/=?^_${'`'}{|}~`;
+
+const generateToken = (key) => {
+    let token = '';
+    for(let i = 0; i < key.length; i++){
+        let index = char.indexOf(key[i]) || char.length / 2;
+        let randomIndex = Math.floor(Math.random() * index);
+        token += char[randomIndex] + char[index - randomIndex];
+    }
+    
+    return token;
+}
+
+const compareToken = (token, key) => {
+    let string = '';
+    for(let i = 0; i < token.length; i=i+2){
+        let index1 = char.indexOf(token[i]);
+        let index2 = char.indexOf(token[i+1]);
+        string += char[index1 + index2];
+    }
+    if(string === key){
+        return true;
+    }
+    return false;
+}
+
+// common functions
+// send data function
+const sendData = async (path, data) => {
+    try{
+        let res = await fetch(path, {
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify(data)
+        })
+
+        let response = await res.json()
+        processData(response);
+        if (await res.status == 200){
+            return response
+        }
+        else {
+            return false
+        }
+        // fetch(path, {
+        //     method: 'post',
+        //     headers: new Headers({'Content-Type': 'application/json'}),
+        //     body: JSON.stringify(data)
+        // }).then((res) => res.json())
+        // .then(response => {
+        //     processData(response);
+        // })
+    }
+    catch{
+        return false
+    }
+}
+
+const processData = (data) => {
+    loader.style.display = null;
+    if (data.alert) {
+        showAlert(data.alert);
+    } else if(data.name) {
+        // create authToken
+        data.authToken = generateToken(data.email);
+        // store authToken in localStorage
+        sessionStorage.user = JSON.stringify(data);
+        // redirect to home page
+        location.replace('/');
+    } else if(data == true){
+        // seller page
+        let user = JSON.parse(sessionStorage.user);
+        user.seller = true;
+        sessionStorage.user = JSON.stringify(user);
+        location.reload();
+    } else if(data.product){
+        location.href = '/seller';
+    }
+}
+
+// alert function
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000);
+    return false;
+}
+
+// UPDATES: Pencode
+// Send FormData -------------------------
+const sendFormData = async (path, formData) => {
+    fetch(path, {
+        method: 'post',
+        // headers: { "Content-Type": "multipart/form-data" },
+        body: formData
+    })
+    .then((res) => res.json())
+    .then(json => {
+        console.log(json)
+    })
+    .catch(ex => {
+        console.log(ex)
+    })
+}
+
+// UPDATES: Pencode
+const getProductImages = async (product) => {
+    try{
+        let fetchtRes = await fetch('/get-product-images', {
+                            method: 'POST',
+                            headers: new Headers({"Content-Type": "application/json"}),
+                            body: JSON.stringify({product: product.id})
+                        })
+    
+        if (fetchtRes.status == 200){
+            let jsonRes = await fetchtRes.json()
+            let product_images = jsonRes['content']
+            for (let i = 0; i < product_images.length; i++){
+                product[`img_${i}`] = product_images[i]
+            }
+            return product_images;
+        }
+    }
+    catch(e){
+        console.error(e)
+    }
+}
+// ----------------------------------------
